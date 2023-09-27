a04f97bb532a3534b1d9dfe7d508d81c3f41c623
lensonfernandes <lenson.fernand@gmail.com>
2023-09-27 23:16:10 +0530

    PORT - Hero Section code updated

--- portfolio/app/components/HeroSection.jsx
+++ portfolio/app/components/HeroSection.jsx
@@ -4,19 +4,23 @@ import Image from "next/image";
 const HeroSection = () => {
   return (
     <section>
-      <div>
-        <div>
-          <h1 className="text-white mb-4 text-4xl ">Hi, Im lenson</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center ">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl ">
+            Hi, Im lenson
+          </h1>
           <p className="text-[#ADA7BE]">NextJs and react Developer</p>
           <div>
-            <button>Hire me </button>
             <button className="px-6 py-3  rounded-full bg-[#FFC700] text-black ml-4 hover:bg-[#FFC700] hover:text-white transition-all duration-300 ease-in-out  hover:scale-105 hover:rotate-12  ">
-                 Download CV</button>
+              Hire me
+            </button>
+            <button className="px-6 py-3  rounded-full bg-[#FFC700] text-black ml-4 hover:bg-[#FFC700] hover:text-white transition-all duration-300 ease-in-out  hover:scale-105 hover:rotate-12  ">
+              Download CV
+            </button>
           </div>
         </div>
         <div>
           <div>
-            {" "}
             <Image
               src=""
               width={500}
