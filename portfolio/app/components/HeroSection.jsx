b8ddce37b1a78de4cdbfe1e0090b683ebe25a7cd
lensonfernandes <lenson.fernand@gmail.com>
2023-09-28 21:06:07 +0530

    PORT - Updated Hero Section Image component

--- portfolio/app/components/HeroSection.jsx
+++ portfolio/app/components/HeroSection.jsx
@@ -38,14 +38,14 @@ const HeroSection = () => {
             </button>
           </div>
         </div>
-        <div>
-          <div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="bg-[#181818] lg:w-[400px] lg:h-[400px] w-[250px] h-[250px] rounded-full relative">
             <Image
-              src=""
-              width={500}
+              src="/images/avatarLen.png"
+              width={300}
               height={300}
               alt="Hero Image"
-              className="rounded-full"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full"
             />
           
         </div>
