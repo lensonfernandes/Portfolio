4c0ee190481825e260ab899f415bc02adb1d5cad
lensonfernandes <lenson.fernand@gmail.com>
2023-09-28 21:18:12 +0530

    PORT -NavBar component added

--- portfolio/app/components/NavLink.jsx
+++ portfolio/app/components/NavLink.jsx
@@ -1,10 +1,14 @@
-import Link from 'next/link'
-import React from 'react'
-
-const NavLink = ({href, title}) => {
+import Link from "next/link";
+const NavLink = ({ href, title }) => {
   return (
-    <Link href={href} className=''> {title}</Link>
-  )
-}
+    <Link
+      href={href}
+      class="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:bg-transparent  md:p-0 hover:text-white "
+      aria-current="page"
+    >
+      {title}
+    </Link>
+  );
+};
 
-export default NavLink
+export default NavLink;
--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -4,11 +4,11 @@ import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col container mx-auto pu-4 px-12">
-      <div className="container mx-auto px-12 py-4">
-        <HeroSection />
-      </div>
-      <Navbar />
-    </main>
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+    <Navbar />
+    <div className="container mx-auto mt-24 px-12 py-4">
+      <HeroSection />
+    </div>
+  </main>
   );
 }
