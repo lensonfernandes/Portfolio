4ba5159934c60791c049f921366edbe02857111f
lensonfernandes <lenson.fernand@gmail.com>
2023-09-28 21:30:21 +0530

    PORT -About Section Added

---
+++ portfolio/app/components/AboutSection.jsx
@@ -0,0 +1,94 @@
+"use client";
+import React, { useState, useTransition } from "react";
+import Image from "next/image";
+import TabButton from "./tabButton";
+// import TabButton from "./TabButton";
+
+
+const TAB_DATA = [
+    {
+      title: "Skills",
+      id: "skills",
+      content: (
+        <ul className="list-disc pl-2">
+          <li>NextJs</li>
+          <li>ReactJS</li>
+          <li>JavaScript</li>
+          <li>Tailwind</li>
+          <li>CSS</li>
+          <li>HTML</li>
+        </ul>
+      ),
+    },
+    {
+      title: "Education",
+      id: "education",
+      content: (
+        <ul className="list-disc pl-2">
+          <li>Canara Engineering College</li>
+          <li>St. Aloysius College</li>
+        </ul>
+      ),
+    },
+    {
+      title: "Certifications",
+      id: "certifications",
+      content: (
+        <ul className="list-disc pl-2">
+          <li>Citrix Cloud Fundamentals</li>
+          <li>Citrix Certified Associate - Virtualization</li>
+        </ul>
+      ),
+    },
+  ];
+
+  const AboutSection = () => {
+    const [tab, setTab] = useState("skills");
+    const [isPending, startTransition] = useTransition();
+  
+    const handleTabChange = (id) => {
+      startTransition(() => {
+        setTab(id);
+      });
+    };
+
+
+  return (
+    <section className="text-white">
+      <div className="gap-8 items-center py-8 px-4 xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-white texxt-base md:text-lg">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              Skills
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              Education
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              Certifications
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AboutSection
