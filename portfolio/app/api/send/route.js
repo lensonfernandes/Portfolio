183615ea6f85a9257ace23cc6e4439e723e33012
lensonfernandes <lenson.fernand@gmail.com>
2023-09-29 23:31:10 +0530

    PORT - Commented routejs file in api directory

--- portfolio/app/api/send/route.js
+++ portfolio/app/api/send/route.js
@@ -1,24 +1,24 @@
 // import { EmailTemplate } from '../../../components/EmailTemplate';
-import { NextResponse } from "next/server";
-import { Resend } from "resend";
+// import { NextResponse } from "next/server";
+// import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
-  try {
-    const data = await resend.emails.send({
-      from: 'Lenson <lenson.fernand@gmail.com>',
-      to: ["lensonfernandes27@gmail.com"],
-      subject: "Hello world",
-      react: (
-        <>
-          <p>Email Body</p>
-        </>
-      ),
-    });
+// export async function POST() {
+//   try {
+//     const data = await resend.emails.send({
+//       from: 'Lenson <lenson.fernand@gmail.com>',
+//       to: ["lensonfernandes27@gmail.com"],
+//       subject: "Hello world",
+//       react: (
+//         <>
+//           <p>Email Body</p>
+//         </>
+//       ),
+//     });
 
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json({ error });
-  }
-}
+//     return NextResponse.json(data);
+//   } catch (error) {
+//     return NextResponse.json({ error });
+//   }
+// }
