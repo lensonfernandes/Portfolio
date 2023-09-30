57197efcf0189f232da2cfd6b18cca6c59c6fe4d
lensonfernandes <lenson.fernand@gmail.com>
2023-09-30 16:17:07 +0530

    PORT - About Section completed

--- portfolio/app/components/AboutSection.jsx
+++ portfolio/app/components/AboutSection.jsx
@@ -1,58 +1,93 @@
 "use client";
 import React, { useState, useTransition } from "react";
 import Image from "next/image";
-import TabButton from "@/app/components/TabButton"
-
-
-
+import TabButton from "@/app/components/TabButton";
 
 const TAB_DATA = [
-    {
-      title: "Skills",
-      id: "skills",
-      content: (
-        <ul className="list-disc pl-2">
-          <li>NextJs</li>
-          <li>ReactJS</li>
-          <li>JavaScript</li>
-          <li>Tailwind</li>
-          <li>CSS</li>
-          <li>HTML</li>
-        </ul>
-      ),
-    },
-    {
-      title: "Education",
-      id: "education",
-      content: (
-        <ul className="list-disc pl-2">
-          <li>Canara Engineering College</li>
-          <li>St. Aloysius College</li>
-        </ul>
-      ),
-    },
-    {
-      title: "Certifications",
-      id: "certifications",
-      content: (
-        <ul className="list-disc pl-2">
-          <li>Citrix Cloud Fundamentals</li>
-          <li>Citrix Certified Associate - Virtualization</li>
-        </ul>
-      ),
-    },
-  ];
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      // <ul className="list-disc pl-2">
+      //   <li>NextJs</li>
+      //   <li>ReactJS</li>
+      //   <li>JavaScript</li>
+      //   <li>Tailwind</li>
+      //   <li>CSS</li>
+      //   <li>HTML</li>
+      // </ul>
+      // <ul className="flex flex-wrap gap-2">
+      //   <li className="bg-blue-500 text-white rounded-full px-2 py-1 text-xs">
+      //     Next.js
+      //   </li>
+      //   <li className="bg-green-500 text-white rounded-full px-2 py-1 text-xs">
+      //     React.js
+      //   </li>
+      //   <li className="bg-yellow-500 text-white rounded-full px-2 py-1 text-xs">
+      //     JavaScript
+      //   </li>
+      //   <li className="bg-indigo-500 text-white rounded-full px-2 py-1 text-xs">
+      //     Tailwind CSS
+      //   </li>
+      //   <li className="bg-pink-500 text-white rounded-full px-2 py-1 text-xs">
+      //     CSS
+      //   </li>
+      //   <li className="bg-purple-500 text-white rounded-full px-2 py-1 text-xs">
+      //     HTML
+      //   </li>
+      // </ul>
+      <ul className="flex flex-wrap gap-2">
+  <li className="bg-blue-500 text-white rounded-full px-3 py-2 text-sm">Next.js</li>
+  <li className="bg-green-500 text-white rounded-full px-3 py-2 text-sm">React.js</li>
+  <li className="bg-yellow-500 text-white rounded-full px-3 py-2 text-sm">JavaScript</li>
+  <li className="bg-indigo-500 text-white rounded-full px-3 py-2 text-sm">Tailwind CSS</li>
+  <li className="bg-pink-500 text-white rounded-full px-3 py-2 text-sm">CSS</li>
+  <li className="bg-purple-500 text-white rounded-full px-3 py-2 text-sm">HTML</li>
+</ul>
 
-  const AboutSection = () => {
-    const [tab, setTab] = useState("skills");
-    const [isPending, startTransition] = useTransition();
-  
-    const handleTabChange = (id) => {
-      startTransition(() => {
-        setTab(id);
-      });
-    };
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      // <ul className="list-disc pl-2">
+      //   <li>Canara Engineering College</li>
+      //   <li>St. Aloysius College</li>
+      // </ul>
+      <ul className="flex flex-wrap gap-2">
+  <li className="bg-blue-500 text-white rounded-full px-3 py-2 text-sm">Canara Engineering College</li>
+  <li className="bg-green-500 text-white rounded-full px-3 py-2 text-sm">St. Aloysius College</li>
+</ul>
 
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      // <ul className="list-disc pl-2">
+      //   <li>Citrix Cloud Fundamentals</li>
+      //   <li>Citrix Certified Associate - Virtualization</li>
+      // </ul>
+      <ul className="flex flex-wrap gap-2">
+  <li className="bg-blue-500 text-white rounded-full px-3 py-2 text-sm">Citrix Cloud Fundamentals</li>
+  <li className="bg-green-500 text-white rounded-full px-3 py-2 text-sm">Citrix Certified Associate - Virtualization</li>
+</ul>
+
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
 
   return (
     <section className="text-white " id="about">
@@ -61,7 +96,12 @@ const TAB_DATA = [
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-white text-base md:text-lg ">
-          Hi, I'm Lenson, a web developer skilled in Next.js, React.js, JavaScript, Tailwind CSS, HTML, and CSS. I graduated from Canara Engineering College and St. Aloysius College and have collaborated with companies like Citrix, Webonise, and AccioJob. I'm passionate about crafting user-friendly web solutions and constantly staying updated with the latest trends in web development. Let's build something amazing together!
+            I'm Lenson, a skilled web developer with expertise in Next.js,
+            React.js, JavaScript, Tailwind CSS, HTML, and CSS. I've collaborated
+            with top companies like Citrix, Webonise, and AccioJob. Passionate
+            about crafting user-friendly web solutions and staying updated with
+            the latest web development trends. Let's build something amazing
+            together!
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
@@ -83,13 +123,13 @@ const TAB_DATA = [
               Certifications
             </TabButton>
           </div>
-          <div className="mt-8">
+          <div className="mt-4">
             {TAB_DATA.find((t) => t.id === tab).content}
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default AboutSection
+export default AboutSection;
