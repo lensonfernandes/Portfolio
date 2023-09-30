4324f69d9133b497620acc6bfab69d2b146be6d4
lensonfernandes <lenson.fernand@gmail.com>
2023-09-30 18:53:54 +0530

    HBPD-PORT Added Margin to Footer

--- portfolio/app/components/Footer.jsx
+++ portfolio/app/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+    <footer className="footer border mt-10 z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-        <span>L.</span>
+        <span>Len.</span>
         <p className="text-slate-600">All rights reserved.</p>
       </div>
     </footer>
