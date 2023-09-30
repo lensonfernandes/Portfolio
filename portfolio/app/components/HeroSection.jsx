2ce9b1c2f56dce869427af2aba24c28709ff8455
lensonfernandes <lenson.fernand@gmail.com>
2023-09-30 11:40:12 +0530

    PORT - Dummy commit to Trigger Deployment

--- portfolio/app/components/HeroSection.jsx
+++ portfolio/app/components/HeroSection.jsx
@@ -59,7 +59,7 @@ const HeroSection = () => {
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/lenHero3.jpg"
-              alt="hero image"
+              alt="Hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full"
               width={300}
               height={300}
